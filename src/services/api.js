14c2a3ecacbb70df96919d350bd5a767050af84c
PDurgalalitha <84369173+PDurgalalitha@users.noreply.github.com>
2025-10-27 22:08:22 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -3,6 +3,7 @@ import axios from "axios";
 const API_BASE = process.env.REACT_APP_API_BASE_URL;
 
 export const askQuestion = (question) => {
-  return axios.post(`${API_BASE}/chat`, { question });
+  return axios.post(`${API_BASE}chat`, { question });
 };
+
 
