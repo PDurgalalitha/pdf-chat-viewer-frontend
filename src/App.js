a53cfe53847e5b54d17bbb950351cd1d57efef39
PDurgalalitha <84369173+PDurgalalitha@users.noreply.github.com>
2025-10-27 22:01:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-import { useRef, useState, useEffect } from "react";
+
 import PdfViewer from "./components/MainContainer/index";
-import ChatWindow from "./components/ChatWindow/index";
-import { uploadPdf, askQuestion } from "./services/api";
 
 function App() {
   return <PdfViewer />;
